509d62abafd5d8f2974e87aad0bd30b55e2ddd01
free <csufuyi@gmail.com>
2015-12-09 17:28:27 +0800

    xargs improve

--- gendir.sh
+++ gendir.sh
@@ -1,3 +1,4 @@
-find . -name '*.hpp' -o -name '*.h' -exec dirname {} \; | sort | uniq > temp
-sed -e 's/^/"-I/g' -e 's/$/"/g' -e 's/\./\/Users\/csufuyi\/work\/trunk/g' -e '1s/^/((nil \. ((company-clang-arguments \. (/ '  -e '$s/$/)))))/' temp>.dir-locals.el
+find . -name '*.hpp' -o -name '*.h'|xargs -I% dirname "%"|sort|uniq>temp
+sed -e 's/^/"-I/g' -e 's/$/"/g' -e 's/\./\/data\/home\/yifu\/mtown_160130\/MTownAT\/Bin\/work\/SVN/g' -e '1s/^/((nil \. ((company-clang-arguments \. (/ '  -e '$s/$/)))))/' temp>.dir-locals.el
 rm temp
+
